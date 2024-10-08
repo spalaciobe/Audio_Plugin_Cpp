089c8f6cb5b45eb36dff6a09c610db0140cf258e
spalaciobe <sebaspalacio02@gmail.com>
2024-10-07 20:59:27 -0500

    Added grid labels

--- SimpleEQ/Source/PluginEditor.cpp
+++ SimpleEQ/Source/PluginEditor.cpp
@@ -333,7 +333,7 @@ void ResponseCurveComponent::resized()
     //g.drawRect(getAnalysisArea());
 
 
-    g.setColour(Colours::lightgreen);
+    g.setColour(Colours::lightgrey);
     const int fontHeight = 10;
     g.setFont(fontHeight);
 
@@ -388,6 +388,32 @@ void ResponseCurveComponent::resized()
         g.drawFittedText(str, r, juce::Justification::centred, 1);
     }
 
+    for (auto gDb : gain)
+    {
+        auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
+
+        String str;
+
+        if (gDb > 0.f)
+        {
+            str << "+";
+        }
+        
+        str << gDb;
+
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+
+        Rectangle<int> r;
+
+        r.setSize(textWidth, fontHeight);
+        r.setX(getWidth() - textWidth);
+        r.setCentre(r.getCentreX(),y);
+
+        g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::lightgrey);
+
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+
+    }
 
 }
 
