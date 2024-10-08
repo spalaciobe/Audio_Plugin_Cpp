0f1c569cad7f1ecd33a73f9b72847b6ca24d649f
spalaciobe <sebaspalacio02@gmail.com>
2024-10-08 11:06:04 -0500

    Implemented FFT Spectrum Analyzer

--- SimpleEQ/Source/PluginEditor.cpp
+++ SimpleEQ/Source/PluginEditor.cpp
@@ -156,16 +156,15 @@ juce::String RotarySliderWithLabels::getDisplayString() const
 
 //==============================================================================
 ResponseCurveComponent::ResponseCurveComponent(SimpleEQAudioProcessor& p) : audioProcessor(p),
-leftChannelFifo(&audioProcessor.leftChannelFifo)
+//leftChannelFifo(&audioProcessor.leftChannelFifo)
+leftPathProducer(audioProcessor.leftChannelFifo),
+rightPathProducer(audioProcessor.rightChannelFifo)
 {
     const auto& params = audioProcessor.getParameters();
     for (auto param : params)
     {
         param->addListener(this);
     }
-
-    leftChannelFFTDataGenerator.changeOrder(FFTOrder::order2048);
-    monoBuffer.setSize(1, leftChannelFFTDataGenerator.getFFTSize());
 
     updateChain();
 
@@ -186,7 +185,8 @@ void ResponseCurveComponent::parameterValueChanged(int parameterIndex, float new
     parametersChanged.set(true);
 }
 
-void ResponseCurveComponent::timerCallback()
+
+void PathProducer::process(juce::Rectangle<float> fftBounds, double sampleRate)
 {
     juce::AudioBuffer<float> tempIncomingBuffer;
 
@@ -210,11 +210,11 @@ void ResponseCurveComponent::timerCallback()
             generate a path
     */
 
-    const auto fftBounds = getAnalysisArea().toFloat();
+    //const auto fftBounds = getAnalysisArea().toFloat();
 
     const auto fftSize = leftChannelFFTDataGenerator.getFFTSize();
 
-    const auto binWidth = audioProcessor.getSampleRate() / (double)fftSize;
+    const auto binWidth = sampleRate / (double)fftSize;
 
     while (leftChannelFFTDataGenerator.getNumAvailableFFTDataBlocks() > 0)
     {
@@ -231,15 +231,24 @@ void ResponseCurveComponent::timerCallback()
             display the most recent path
     */
 
-    while (pathProducer.getNumPathsAvailable()) 
+    while (pathProducer.getNumPathsAvailable())
     {
         pathProducer.getPath(leftChannelFFTPath);
     }
+}
+
+
+void ResponseCurveComponent::timerCallback()
+{
+    auto fftBounds = getAnalysisArea().toFloat();
+	auto sampleRate = audioProcessor.getSampleRate();
+
+    leftPathProducer.process(fftBounds, sampleRate);
+    rightPathProducer.process(fftBounds, sampleRate);
 
     if (parametersChanged.compareAndSetBool(false, true))
     {
         updateChain();
-        //repaint();
     }
 
     repaint();
@@ -319,10 +328,19 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
         responseCurve.lineTo(responseArea.getX() + i, map(mags[i]));
     }
 
+    auto leftChannelFFTPath = leftPathProducer.getPath();
+
     leftChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), responseArea.getY()));
 
     g.setColour(Colours::deepskyblue);
     g.strokePath(leftChannelFFTPath, PathStrokeType(1));
+
+	auto rightChannelFFTPath = rightPathProducer.getPath();
+
+    rightChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), responseArea.getY()));
+
+	g.setColour(Colours::tomato);
+	g.strokePath(rightChannelFFTPath, PathStrokeType(1));
 
     g.setColour(Colours::orange);
     g.drawRoundedRectangle(getRenderArea().toFloat(), 4.f, 1.f);
--- SimpleEQ/Source/PluginProcessor.cpp
+++ SimpleEQ/Source/PluginProcessor.cpp
@@ -111,6 +111,13 @@ void SimpleEQAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlo
 
     leftChannelFifo.prepare(samplesPerBlock);
     rightChannelFifo.prepare(samplesPerBlock);
+    
+    osc.initialise([](float x) {return std::sin(x); });
+
+    spec.numChannels = getTotalNumInputChannels();
+    osc.prepare(spec);
+
+    osc.setFrequency(1000);
 
 }
 
@@ -164,6 +171,12 @@ void SimpleEQAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juc
     updateFilters();
 
     juce::dsp::AudioBlock<float> block(buffer);
+
+    //buffer.clear();
+
+    //juce::dsp::ProcessContextReplacing<float> stereoContext(block);
+
+    //osc.process(stereoContext);
 
     auto leftBlock = block.getSingleChannelBlock(0);
     auto rightBlock = block.getSingleChannelBlock(1);
