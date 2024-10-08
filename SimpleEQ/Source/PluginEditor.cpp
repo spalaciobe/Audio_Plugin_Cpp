e3dbcc91b775fd2b33d3bab3c8d3e3be32429f78
spalaciobe <sebaspalacio02@gmail.com>
2024-10-07 20:46:22 -0500

    Added frequency labels

--- SimpleEQ/Source/PluginEditor.cpp
+++ SimpleEQ/Source/PluginEditor.cpp
@@ -332,6 +332,63 @@ void ResponseCurveComponent::resized()
 
     //g.drawRect(getAnalysisArea());
 
+
+    g.setColour(Colours::lightgreen);
+    const int fontHeight = 10;
+    g.setFont(fontHeight);
+
+    Array<float> freqsToWrite
+    {
+        20, 50, 100,
+        200, 500, 1000,
+        2000, 5000, 10000,
+        20000
+    };
+
+    Array<float> xsToWrite;
+    for (auto f : freqsToWrite)
+    {
+        int index = freqs.indexOf(f); 
+        if (index != -1) 
+        {
+            xsToWrite.add(xs[index]); 
+        }
+    }
+
+    for (int i = 0; i < freqsToWrite.size(); ++i)
+    {
+        auto f = freqsToWrite[i];
+        auto x = xsToWrite[i];
+
+        bool addK = false;
+
+        String str;
+
+        if (f > 999.f)
+        {
+            f /= 1000.f;
+            addK = true;
+        }
+
+        str << f;
+        if (addK)
+        {
+            str << "k";
+        }
+        str << "Hz";
+
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+
+        Rectangle<int> r;
+
+        r.setSize(textWidth, fontHeight);
+        r.setCentre(x, 0);
+        r.setY(1);
+
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+    }
+
+
 }
 
 
